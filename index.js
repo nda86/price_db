2bbde16280ea5456ce739a0e91dcf7d8e1ea6133
nda86 <nda86@mail.ru>
2015-10-18 00:21:48 +0800

    full sqlite

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('mydb.db');
-var check;
+var LineByLineReader = require('line-by-line');
+var lr = new LineByLineReader('goods101.txt');
+
+
+var lineByLine = require('n-readlines');
+var liner = new lineByLine('./goods101.txt');
+
 db.serialize(function() {
+	db.run("CREATE TABLE if not exists goods (art TEXT, code TEXT, name TEXT, price TEXT)");
+	var stmt = db.prepare("INSERT INTO goods VALUES (?, ?, ?, ?)");
 
-  db.run("CREATE TABLE if not exists goods (art TEXT, code TEXT, name TEXT, price TEXT)");
-  var stmt = db.prepare("INSERT INTO goods VALUES (?, ?, ?, ?)");
-  for (var i = 0; i < 10; i++) {
-      stmt.run(i, i*2, i*3, i*4);
-  }
-  stmt.finalize();
-
-  // db.each("SELECT rowid AS id, info FROM user_info", function(err, row) {
-  //     console.log(row.id + ": " + row.info);
-  // });
+while (line = liner.next()) {
+	line = line + "";
+	var re = /^\d/i;
+	if (!re.test(line)){continue}
+	var re = /^([^\;]*)\;([^\;]*)\;([^\;]*)\;[^\;]*\;([^\;]*)\;/ig;
+	var res = re.exec(line);
+	// console.log(res);
+	stmt.run(res[1], res[2], res[3], res[4]);
+	// console.log(line);
+}
+	stmt.finalize();
 });
 
 db.close();
+
+
+
+
+
+
+
