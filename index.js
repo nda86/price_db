777f88bdec936872742901a66a772a0989d22ad9
nda <nda86@mail.ru>
2015-11-11 11:37:49 +0800

    office

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database('mydb.db');
+var db = new sqlite3.Database('database.db');
 var LineByLineReader = require('line-by-line');
 var lr = new LineByLineReader('goods101.txt');
 
@@ -8,8 +8,37 @@ var lineByLine = require('n-readlines');
 var liner = new lineByLine('./goods101.txt');
 
 db.serialize(function() {
-	db.run("CREATE TABLE if not exists goods (art TEXT, code TEXT, name TEXT, price TEXT)");
-	var stmt = db.prepare("INSERT INTO goods VALUES (?, ?, ?, ?)");
+	db.run("TRUNCATE TABLE ANALOG;"  
+			+ "TRUNCATE TABLE ASPECT;" 
+			+ "TRUNCATE TABLE BARCODE;" 
+			+ "TRUNCATE TABLE IMAGES;" 
+			+ "TRUNCATE TABLE IMPLEMENTSHEME;" 
+			+ "TRUNCATE TABLE IntBarcs;" 
+			+ "TRUNCATE TABLE REMAIN;" 
+			+ "TRUNCATE TABLE SHEMA;" 
+			+ "TRUNCATE TABLE SPRT;" 
+			+ "TRUNCATE TABLE SPUTNIK;" 
+			+ "TRUNCATE TABLE VALUESHEMA;");
+// 	db.run("CREATE TABLE if not exists goods (art TEXT, code TEXT, name TEXT, price TEXT)");
+// 	var stmt = db.prepare("INSERT INTO BARCODE (BARCODE) VALUES (?)");
+
+// while (line = liner.next()) {
+// 	line = line + "";
+// 	var re = /^\d/i;
+// 	if (!re.test(line)){continue}
+// 	var re = /^([^\;]*)\;([^\;]*)\;([^\;]*)\;[^\;]*\;([^\;]*)\;/ig;
+// 	var res = re.exec(line);
+// 	// console.log(res);
+// 	// res[1] - artikul
+// 	// res[2] - code
+// 	// res[3] - name
+// 	// res[4] - price
+// 	stmt.run(res[2]);
+// 	// console.log(line);
+// }
+
+var stmt1 = db.prepare("INSERT INTO BARCODE (BARCODE) VALUES (?)");
+var stmt2 = db.prepare("INSERT INTO SPRT (NAME, PRICE) VALUES (?, ?)");
 
 while (line = liner.next()) {
 	line = line + "";
@@ -18,10 +47,18 @@ while (line = liner.next()) {
 	var re = /^([^\;]*)\;([^\;]*)\;([^\;]*)\;[^\;]*\;([^\;]*)\;/ig;
 	var res = re.exec(line);
 	// console.log(res);
-	stmt.run(res[1], res[2], res[3], res[4]);
+	// res[1] - artikul
+	// res[2] - code
+	// res[3] - name
+	// res[4] - price
+	stmt1.run(res[2]);
+	stmt2.run(res[3],res[4]);
+
 	// console.log(line);
 }
-	stmt.finalize();
+
+	stmt1.finalize();
+	stmt2.finalize();
 });
 
 db.close();
